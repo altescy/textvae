d558ecb19541c25fb74481e4d112919f17e4bf11
altescy <altescy@fastmail.com>
2022-10-10 18:19:00 +0900

    modify test case

--- tests/data/test_datamodule.py
+++ tests/data/test_datamodule.py
@@ -7,6 +7,6 @@ def test_textvae_datamodule() -> None:
     dataloader = datamodule.build_dataloader(dataset, batch_size=2)
     batch = next(iter(dataloader))
     assert isinstance(batch, Batch)
-    assert batch.tokens.size() == (2, 5)
-    assert batch.mask.size() == (2, 5)
+    assert batch.tokens.size() == (2, 6)
+    assert batch.mask.size() == (2, 6)
     assert len(datamodule.vocab) == 13
